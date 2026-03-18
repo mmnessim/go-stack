3f6e765ee7a6de0c4e0e345453527bb8587e9e28
Mounir Nessim <mmnessim@gmail.com>
2026-03-18 12:18:43 -0400

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mmnessim/go-stack
+
+go 1.25.5
---
+++ stack/stack.go
@@ -0,0 +1,42 @@
+package stack
+
+import "errors"
+
+const CAPACITY = 256
+
+type Stack struct {
+	Items [CAPACITY]int
+	Top   uint
+}
+
+func New() *Stack {
+	return &Stack{Top: 0}
+}
+
+func (s *Stack) Push(val int) error {
+	if s.Top >= CAPACITY {
+		return ErrStackOverflow
+	}
+	s.Items[s.Top] = val
+	s.Top += 1
+	return nil
+}
+
+func (s *Stack) Pop() (int, error) {
+	if s.Top < 2 {
+		return 0, ErrStackUnderflow
+	}
+	val := s.Items[s.Top-1]
+	s.Top -= 1
+	return val, nil
+}
+
+func (s *Stack) Peek() (int, error) {
+	if s.Top < 2 {
+		return 0, ErrStackUnderflow
+	}
+	return s.Items[s.Top-1], nil
+}
+
+var ErrStackOverflow = errors.New("Stack overflow")
+var ErrStackUnderflow = errors.New("Stack underflow")
